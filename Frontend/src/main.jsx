c7748b3b1f64b8a2d01f174c2b716a845c81a447
Nathan Cloutier <nathanc4571@gmail.com>
2025-05-27 09:27:09 -0400

    Website Stuff

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
+
+const matomoInstance = createInstance({
+  urlBase: 'https://matomo.rathacks.com',
+  siteId: 1,
+  trackerUrl: 'https://matomo.rathacks.com/matomo.php',
+  srcUrl: 'https://matomo.rathacks.com/matomo.js',
+})
 
 createRoot(document.getElementById('root')).render(
+  <MatomoProvider value={matomoInstance}>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </MatomoProvider>
 )
