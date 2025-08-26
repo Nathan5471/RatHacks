4919da6f68a95ce4efcec42d066efae806e58018
Nathan Cloutier <nathanc4571@gmail.com>
2025-08-26 19:38:28 -0400

    Setup backend

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+const corsOptions = {
+  origin: true,
+  credentials: true,
+}; // IMPORTANT REMINDER: Change before releasing
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+const MONGODB_URL = process.env.MONGODB_URL;
+if (!MONGODB_URL) {
+  throw new Error("MONGODB_URL is not defined");
+}
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
