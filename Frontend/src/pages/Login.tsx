b161f51f2cf0e5a1f682a2a81f6c81cb472598f9
Nathan Cloutier <nathanc4571@gmail.com>
2025-09-10 20:30:17 -0400

    Improve login and register pages

--- Frontend/src/pages/Login.tsx
+++ Frontend/src/pages/Login.tsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import { login } from "../utils/AuthAPIHandler";
+import { IoEye, IoEyeOff } from "react-icons/io5";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState("");
   const navigate = useNavigate();
   const { getUser } = useAuth();
 
-  const handleLogin = async () => {
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
     setError("");
     if (!email) {
       setError("Email is required");
@@ -37,34 +40,61 @@ export default function Login() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen">
-      <h1 className="text-4xl font-bold mb-4">Login</h1>
-      <div className="w-80 bg-gray-200 flex flex-col items-center justify-center p-2">
-        <div className="flex flex-row">
-          <h3>Email:</h3>
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen bg-surface-a0 text-white">
+      <form
+        className="flex flex-col w-80 bg-surface-a1 rounded-lg p-4 m-4"
+        onSubmit={(e) => handleLogin(e)}
+      >
+        <h1 className="text-4xl font-bold text-center mb-4 text-primary-a0">
+          Login
+        </h1>
+        <label htmlFor="email" className="text-2xl">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="bg-surface-a2 p-2 rounded-lg mt-1"
+          placeholder="Enter your email"
+          required
+        />
+        <label htmlFor="password" className="text-2xl mt-2">
+          Password
+        </label>
+        <div className="flex flex-row w-full mt-1">
           <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-300 p-2 rounded-md"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div className="flex flex-row">
-          <h3>Password:</h3>
-          <input
-            type="text"
+            type={showPassword ? "text" : "password"}
+            id="password"
+            name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-300 p-2 rounded-md"
+            className="bg-surface-a2 p-2 rounded-lg w-full"
             placeholder="Enter your password"
+            required
           />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="ml-2 bg-surface-a2 rounded-lg hover:bg-surface-a3 p-2"
+          >
+            {showPassword ? (
+              <IoEyeOff className="text-xl" />
+            ) : (
+              <IoEye className="text-xl" />
+            )}
+          </button>
         </div>
-        {error && <p className="text-red-500">{error}</p>}
-        <button onClick={handleLogin} className="bg-gray-400 p-2 rounded-md">
+        {error && <p className="text-red-500 mt-2">{error}</p>}
+        <button
+          type="submit"
+          className="bg-surface-a2 p-2 mt-4 rounded-lg hover:bg-surface-a3"
+        >
           Login
         </button>
-      </div>
+      </form>
     </div>
   );
 }
--- Frontend/src/pages/Register.tsx
+++ Frontend/src/pages/Register.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { register } from "../utils/AuthAPIHandler";
+import { IoEye, IoEyeOff } from "react-icons/io5";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [error, setError] = useState("");
@@ -35,64 +38,110 @@ export default function Register() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen">
-      <h1 className="text-4xl font-bold mb-4">Register</h1>
-      <div className="w-80 bg-grayt-200 flex flex-col items-center justify-center p-2">
-        <div className="flex flex-row">
-          <h3>Email:</h3>
+    <div className="flex flex-col items-center justify-center min-h-screen w-screen bg-surface-a0 text-white">
+      <form className="flex flex-col w-80 bg-surface-a1 p-4 m-4 rounded-lg">
+        <h1 className="text-primary-a0 text-4xl font-bold text-center mb-4">
+          Register
+        </h1>
+        <label htmlFor="email" className="text-2xl mt-2">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="bg-surface-a2 p-2 rounded-lg mt-1"
+          placeholder="Enter your email"
+          required
+        />
+        <label htmlFor="firstName" className="text-2xl mt-2">
+          First Name
+        </label>
+        <input
+          type="text"
+          id="firstName"
+          name="lastName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          className="bg-surface-a2 p-2 rounded-lg mt-1"
+          placeholder="Enter your first name"
+          required
+        />
+        <label htmlFor="lastName" className="text-2xl mt-2">
+          Last Name
+        </label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          className="bg-surface-a2 p-2 rounded-lg mt-1"
+          placeholder="Enter your last name"
+          required
+        />
+        <label htmlFor="password" className="text-2xl mt-2">
+          Password
+        </label>
+        <div className="flex flex-row w-full mt-1">
           <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="bg-gray-300 p-2 rounded-md"
-            placeholder="Enter your email"
-          />
-        </div>
-        <div className="flex flex-row">
-          <h3>First Name:</h3>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            className="bg-gray-300 p-2 rounded-md"
-            placeholder="Enter your first name"
-          />
-        </div>
-        <div className="flex flex-row">
-          <h3>Last Name:</h3>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            className="bg-gray-300 p-2 rounded-md"
-            placeholder="Enter your last name"
-          />
-        </div>
-        <div className="flex flex-row">
-          <h3>Password:</h3>
-          <input
-            type="text"
+            type={showPassword ? "text" : "password"}
+            id="password"
+            name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="bg-gray-300 p-2 rounded-md"
+            className="bg-surface-a2 p-2 rounded-lg w-full"
             placeholder="Enter your password"
+            required
           />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="ml-2 bg-surface-a2 rounded-lg hover:bg-surface-a3 p-2"
+          >
+            {showPassword ? (
+              <IoEyeOff className="text-xl" />
+            ) : (
+              <IoEye className="text-xl" />
+            )}
+          </button>
         </div>
-        <div className="flex flex-row">
-          <h3>Confirm Password:</h3>
+        <label htmlFor="confirmPassword" className="text-2xl mt-2">
+          Confirm Password
+        </label>
+        <div className="flex flex-row w-full mt-1">
           <input
-            type="text"
+            type={showConfirmPassword ? "text" : "password"}
+            id="confirmPassword"
+            name="confirmPassword"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
-            className="bg-gray-300 p-2 rounded-md"
+            className="bg-surface-a2 p-2 rounded-lg w-full"
             placeholder="Confirm your password"
+            required
           />
+          <button
+            type="button"
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+            className="ml-2 bg-surface-a2 rounded-lg hover:bg-surface-a3 p-2"
+          >
+            {showConfirmPassword ? (
+              <IoEyeOff className="text-xl" />
+            ) : (
+              <IoEye className="text-xl" />
+            )}
+          </button>
         </div>
         {error && <p className="text-red-500">{error}</p>}
-        <button onClick={handleRegister} className="bg-gray-400 p-2 rounded-md">
+        <button
+          onClick={handleRegister}
+          className="bg-surface-a2 p-2 rounded-lg mt-4 hover:bg-surface-a3"
+        >
           Register
         </button>
-      </div>
+      </form>
     </div>
   );
 }
