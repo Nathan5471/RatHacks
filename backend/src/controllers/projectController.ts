06bd8287a0d310ce81b4f242c181c3343b760c6d
Nathan Cloutier <nathanc4571@gmail.com>
2025-10-04 16:30:37 -0400

    Add getting and updating project to backend

--- backend/src/controllers/projectController.ts
+++ backend/src/controllers/projectController.ts
@@ -44,14 +44,119 @@ export const createProject = async (req: any, res: any) => {
         teamId: team.id,
       },
     });
-    return res
-      .status(201)
-      .json({
-        message: "Project created successfully",
-        projectId: newProject.id,
-      });
+    return res.status(201).json({
+      message: "Project created successfully",
+      projectId: newProject.id,
+    });
   } catch (error) {
     console.error("Error creating project:", error);
     return res.status(500).json({ message: "Failed to create project" });
+  }
+};
+
+export const updateProject = async (req: any, res: any) => {
+  const { projectId } = req.params as { projectId: string };
+  const { name, description, codeURL, demoURL } = req.body as {
+    name: string;
+    description: string;
+    codeURL: string | null;
+    demoURL: string | null;
+  };
+  const screenshot = req.screenshot as string | null;
+  const video = req.video as string | null;
+
+  try {
+    const project = await prisma.project.findUnique({
+      where: { id: projectId },
+    });
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    await prisma.project.update({
+      where: { id: project.id },
+      data: {
+        name,
+        description,
+        codeURL,
+        demoURL,
+      },
+    });
+    if (screenshot) {
+      await prisma.project.update({
+        where: { id: project.id },
+        data: {
+          screenshotPath: screenshot,
+        },
+      });
+    }
+    if (video) {
+      await prisma.project.update({
+        where: { id: project.id },
+        data: {
+          videoPath: video,
+        },
+      });
+    }
+    return res.status(200).json({ message: "Event upadted sucessfully" });
+  } catch (error) {
+    console.error("Error updating project:", error);
+    return res.status(500).json({ message: "Failed to update project" });
+  }
+};
+
+export const getProjectByEventId = async (req: any, res: any) => {
+  const { eventId } = req.params as { eventId: string };
+  const user = req.user as User;
+
+  try {
+    const event = await prisma.event.findUnique({
+      where: { id: eventId },
+    });
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+    const team = await prisma.team.findFirst({
+      where: {
+        members: { has: user.id },
+        eventId: event.id,
+      },
+    });
+    if (!team) {
+      return res
+        .status(403)
+        .json({ message: "User is not part of any team for this event" });
+    }
+
+    const project = await prisma.project.findFirst({
+      where: {
+        eventId: event.id,
+        teamId: team.id,
+      },
+    });
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    return res.status(200).json({
+      message: "Project loaded succesfully",
+      project: {
+        id: project.id,
+        name: project.name,
+        description: project.description,
+        codeURL: project.codeURL,
+        screenshotURL: project.screenshotPath
+          ? `/api/uploads/${project.screenshotPath}`
+          : null,
+        videoURL: project.videoPath
+          ? `/api/uploads/${project.videoPath}`
+          : null,
+        demoURL: project.demoURL,
+        submittedAt: project.submittedAt,
+      },
+    });
+  } catch (error) {
+    console.log("Error getting project by event ID:", error);
+    return res.status(500).json({ message: "Failed to get event" });
   }
 };
--- backend/src/routes/projectRouter.ts
+++ backend/src/routes/projectRouter.ts
@@ -1,5 +1,9 @@
 import express from "express";
-import { createProject } from "../controllers/projectController";
+import {
+  createProject,
+  updateProject,
+  getProjectByEventId,
+} from "../controllers/projectController";
 import authenticate from "../middleware/authenticate";
 import upload from "../middleware/upload";
 
@@ -35,5 +39,45 @@ router.post(
     await createProject(req, res);
   }
 );
+
+router.put(
+  "/update/:projectId",
+  authenticate,
+  upload.fields([
+    { name: "screenshot", maxCount: 1 },
+    { name: "video", maxCount: 1 },
+  ]),
+  async (req: any, res: any) => {
+    const { projectId } = req.params as { projectId: string };
+    const { name, description, codeURL, demoURL } = req.body as {
+      name: string;
+      description: string;
+      codeURL: string | null;
+      demoURL: string | null;
+    };
+    if (!projectId || !name || !description) {
+      return res
+        .status(400)
+        .json({ message: "Project ID, Name, and Description are required" });
+    }
+
+    req.screenshot = req.files?.screenshot
+      ? req.files.screenshot[0].filename
+      : null;
+    req.video = req.files?.video ? req.files.video[0].filename : null;
+
+    await updateProject(req, res);
+  }
+);
+
+router.get("/get/:eventId", authenticate, async (req: any, res: any) => {
+  const { eventId } = req.params as { eventId: string };
+
+  if (!eventId) {
+    return res.status(400).json({ message: "Event ID is required" });
+  }
+
+  await getProjectByEventId(req, res);
+});
 
 export default router;
